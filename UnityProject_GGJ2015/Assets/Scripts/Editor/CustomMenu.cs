b60a628aa5b50169d49880ff42325eb9ade42037
Simon Albou <albou.simon@gmail.com>
2015-01-23 20:53:03 +0100

    Création du projet Unity + du CustomMenu

---
+++ UnityProject_GGJ2015/Assets/Scripts/Editor/CustomMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CustomMenu : Editor {
+
+	[MenuItem("Custom/Prepare Scene")]
+	public static void PrepareScene()
+	{
+		Debug.Log ("Preparing the scene...");
+
+		GameObject[] all = GameObject.FindObjectsOfType(typeof (GameObject)) as GameObject[];
+
+		foreach(GameObject obj in all)
+		{
+			Debug.Log ("...C'est bien gentil mais il n'y a rien à faire ici encore");
+		}
+
+		Debug.Log ("Done !");
+	}
+}
