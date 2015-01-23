d70bd17e9fa12afb3514e9554c348b34269fe734
Simon Albou <albou.simon@gmail.com>
2015-01-23 21:15:06 +0100

    Key Binding

---
+++ UnityProject_GGJ2015/Assets/Scripts/KeyBinding.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBinding : MonoBehaviour {
+
+	public static KeyCode[] leftKeys = { KeyCode.LeftArrow, KeyCode.Q, KeyCode.A };
+	public static KeyCode[] rightKeys = { KeyCode.RightArrow, KeyCode.D };
+	public static KeyCode[] upKeys = { KeyCode.UpArrow, KeyCode.Z, KeyCode.W };
+	public static KeyCode[] downKeys = { KeyCode.DownArrow, KeyCode.S };
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
