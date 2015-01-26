9fe9a682f904491294d6460261af635a2c276e67
Simon Albou <albou.simon@gmail.com>
2015-01-26 19:50:39 +0100

    Indicateur du délai

---
+++ UnityProject_GGJ2015/Assets/Scripts/UI_Info.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_Info : MonoBehaviour {
+
+	public Text self;
+
+	// Use this for initialization
+	void Start () {
+		self.text = "Delay : "+Controller.staticMem.ToString()+" turn";
+		if(Controller.staticMem>1) self.text += "s";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
